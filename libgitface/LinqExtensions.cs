82aac8900a43946892d82cce89a5ac0155558dea
Alan McGovern <alan@xamarin.com>
2018-04-01 23:46:27 +0100

    Add a linq extension to Distinct objects using a Func

---
+++ libgitface/LinqExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace libgitface
+{
+	public static class LinqExtensions
+	{
+		class DelegateComparer<T, TCompare> : IEqualityComparer<T>
+		{
+			Func<T, TCompare> Selector;
+
+			public DelegateComparer (Func<T, TCompare> selector) => Selector = selector;
+
+			public bool Equals (T x, T y) => EqualityComparer<TCompare>.Default.Equals (Selector (x), Selector (y));
+
+			public int GetHashCode (T obj) => EqualityComparer<TCompare>.Default.GetHashCode (Selector (obj));
+		}
+
+		public static IEnumerable<T> Distinct<T, TCompare> (this IEnumerable<T> self, Func<T, TCompare> selector)
+		{
+			var comparer = new DelegateComparer<T, TCompare> (selector);
+			return self.Distinct (comparer);
+		}
+	}
+}
