268b70bac728eb46a331f3ef9aadc79aa31330fa
Jérémie Laval <jeremie.laval@gmail.com>
2019-03-20 12:10:56 -0400

    Add missing file

---
+++ libgitface/GitClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace libgitface
+{
+	public static class GitClientExtensions
+	{
+		public static async Task CreateAndOpenPullRequest (this GitClient client, string branchName, string titleText, string bodyText, bool openPrInBrowser = true)
+		{
+			var prUrl = await client.CreatePullRequest (branchName, titleText, bodyText);
+			if (openPrInBrowser)
+				new OpenUrlAction { Url = prUrl }.Execute ();
+		}
+	}
+}
